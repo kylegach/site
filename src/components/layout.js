f9c5948c2e9fcafdb5e0b409d1c96686b5ab673a
kylegach <kyle.gach@ca.com>
2018-08-28 22:32:53 -0600

    define custom components for mdx content; add mdx runtime
    - runtime enables parsed markdown inside components in mdx content 
    (without custom components)

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { Provider } from 'rebass'
+import { MDXProvider } from '@mdx-js/tag'
+import { Provider as RebassProvider, Heading, Text, Link } from 'rebass'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
 import './layout.css'
+
+const components = {
+  a: props => <Link is="a" {...props} />,
+  h1: props => <Heading is="h1" fontSize={7} mb={3} mt={5} {...props} />,
+  h2: props => <Heading is="h2" fontSize={5} mb={3} mt={4} {...props} />,
+  h3: props => <Heading is="h3" fontSize={3} mb={3} mt={3} {...props} />,
+  p: props => <Text is="p" mb={3} {...props} />,
+}
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -29,7 +38,7 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Provider>
+        <RebassProvider>
           <Header siteTitle={data.site.siteMetadata.title} />
           <div
             style={{
@@ -39,9 +48,9 @@ const Layout = ({ children }) => (
               paddingTop: 0,
             }}
           >
-            {children}
+            <MDXProvider components={components}>{children}</MDXProvider>
           </div>
-        </Provider>
+        </RebassProvider>
       </>
     )}
   />
